7b053e9172da2602dc9dd3ae113a4a2bf85db6b1
BA <brad.adams001@umb.edu>
2018-11-10 18:00:44 -0500

    Added MRMP.sol

---
+++ contracts/MRMP.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.4.24;
+
+import 'https://github.com/bradradams/mrmp/blob/master/contracts/RMP721.sol';
+
+/**
+ *
+ * Music Royalty Management Platform
+ * Uses RMP721 Non-Fungible Token (Based on ERC721 standard)
+ *
+ */
+
+contract MRMP {
+  constructor() {
+  }
+}
