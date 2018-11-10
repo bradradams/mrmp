f981f64b76b5d73835e97f4121f752147a743dc0
BA <brad.adams001@umb.edu>
2018-11-10 18:06:38 -0500

    Update

--- contracts/RMP721.sol
+++ contracts/RMP721.sol
@@ -3,6 +3,12 @@ pragma solidity ^0.4.24;
 import 'openzeppelin-solidity/contracts/token/ERC721/ERC721Full.sol';
 import 'openzeppelin-solidity/contracts/token/ERC721/ERC721Mintable.sol';
 
+/**
+ *
+ * RMP721 Non-Fungible Token (Based on ERC721 standard)
+ *
+ */
+
 contract RMP721 is ERC721Full, ERC721Mintable {
   constructor() ERC721Full("RMP721", "RMP") public {
   }
